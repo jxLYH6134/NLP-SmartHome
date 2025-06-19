6ceea21ae95c231453315fc33fd98e3967b1f0f0
桜与白 <318099184@qq.com>
2025-06-19 20:43:09 +0800

    init 整体结构

---
+++ pages/device/device.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<view class="edgeInsetTop"></view>
+		<view class="edgeInsetBottom"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.edgeInsetTop {
+		width: 750rpx;
+		height: var(--window-top);
+	}
+
+	.edgeInsetBottom {
+		width: 750rpx;
+		height: var(--window-bottom);
+	}
+</style>
